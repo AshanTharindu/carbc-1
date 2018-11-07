dc3b7be85fefe8528ccd8e0d5ad7c6933e008a03
AshanTharindu <atharindu94@gmail.com>
2018-11-07 17:21:50 +0530

    exchange ownership test change

--- src/main/java/TestCases/ExchangeOwnershipTest.java
+++ src/main/java/TestCases/ExchangeOwnershipTest.java
@@ -17,13 +17,13 @@ public class ExchangeOwnershipTest {
         Controller controller = new Controller();
         controller.startNode();
 //        MessageSender.requestIP();
-        controller.sendTransaction("ExchangeOwnership", "V-1234", createRegisterJSON() );
+        controller.sendTransaction("ExchangeOwnership", "V -92961", createRegisterJSON() );
     }
 
     public static JSONObject createRegisterJSON(){
         JSONObject ownership = new JSONObject();
         ownership.put("registrationNumber", "CAR-12345");
-        ownership.put("vehicleId", "CAR-12345");
+        ownership.put("vehicleId", "V -92961");
         ownership.put("preOwner", "3081f13081a806072a8648ce38040130819c024100fca682ce8e12caba26efccf7110e526db078b05edecbcd1eb4a208f3ae1617ae01f35b91a47e6df63413c5e12ed0899bcd132acd50d99151bdc43ee737592e17021500962eddcc369cba8ebb260ee6b6a126d9346e38c50240678471b27a9cf44ee91a49c5147db1a9aaf244f05a434d6486931d2d14271b9e35030b71fd73da179069b32e2935630e1c2062354d0da20a6c416e50be794ca4034400024100b4e8f0dc4e2d34820dcbb0218437ef914cfcfdb79fc3161ae9a6453381251ddc1c95d11780aff761761a4168b753cf335a016c662e54a2b96267a7727697616b");
         JSONObject secondaryParty = new JSONObject();
         JSONObject newOwner = new JSONObject();
