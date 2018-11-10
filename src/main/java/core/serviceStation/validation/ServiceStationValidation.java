12a1ecf1efe6599d95f962cef6f0c86df2229d1f
sajiniekavindya <sajiniekavindya@gmail.com>
2018-11-10 15:04:07 +0530

    changing serviceStationValidation class

--- src/main/java/core/serviceStation/validation/ServiceStationValidation.java
+++ src/main/java/core/serviceStation/validation/ServiceStationValidation.java
@@ -3,7 +3,6 @@ package core.serviceStation.validation;
 import core.blockchain.Block;
 import core.consensus.Consensus;
 import core.serviceStation.dao.ServiceJDBCDAO;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.sql.SQLException;
@@ -11,25 +10,16 @@ import java.sql.SQLException;
 public class ServiceStationValidation {
 
     public static boolean validateBlock(Block block) {
-        System.out.println("inside ServiceStationValidation/validateBlock");
+        System.out.println("----------------------Begin of ServiceStationValidation/validateBlock---------------------------------");
+
         try {
             String serviceData = ServiceJDBCDAO.getInstance().getLastServiceRecord(block.getBlockBody().getTransaction().getAddress()).toString();
 
             System.out.println(block.getBlockBody().getTransaction().getData());
             System.out.println(serviceData);
 
-            System.out.println("just before sending agreement");
-
             JSONObject dataFromBlock = new JSONObject(block.getBlockBody().getTransaction().getData());
-            JSONObject dataFromServiceStation = new JSONObject(block.getBlockBody().getTransaction().getData());
-
-            System.out.println("--------------------------------------------------------------------------");
-            System.out.println(dataFromBlock.getString("vehicle_id"));
-            System.out.println(dataFromServiceStation.getString("vehicle_id"));
-            System.out.println("--------------------------------------------------------------------------");
-            System.out.println(dataFromBlock.getJSONArray("services"));
-            System.out.println(dataFromServiceStation.getJSONArray("services"));
-            System.out.println("--------------------------------------------------------------------------");
+            JSONObject dataFromServiceStation = new JSONObject(serviceData);
 
             if (dataFromBlock.getString("serviced_date").equals(dataFromServiceStation.getString("serviced_date")) &&
                     dataFromBlock.getInt("cost")== (dataFromServiceStation.getInt("cost")) &&
@@ -37,33 +27,17 @@ public class ServiceStationValidation {
                     (dataFromBlock.getJSONArray("services").toString()).equals((dataFromServiceStation.getJSONArray("services")).toString())){
 
 
-                System.out.println("service data is true; validated by service station");
+                System.out.println("Service data is true. Validated by Service Station");
                 Consensus.getInstance().sendAgreementForBlock(block.getBlockHeader().getHash());
 
-//                JSONArray servicesFromBlock = dataFromBlock.getJSONArray("services");
-//                JSONArray servicesFromStation = dataFromBlock.getJSONArray("services");
-//
-//                for (int i = 0; i < servicesFromBlock.length(); i++){
-//                    JSONObject serviceItemFromBlock = servicesFromBlock.getJSONObject(i);
-//                    JSONObject serviceItemFromStation = servicesFromBlock.getJSONObject(i);
-//
-//                    if (serviceItemFromBlock.getString("serviceType").equals(serviceItemFromStation.getString("serviceType")))
-//                }
-
                 return true;
-
             }
-
-//            if(block.getBlockBody().getTransaction().getData().equals(serviceData)) {
-//                Consensus.getInstance().sendAgreementForBlock(block.getBlockHeader().getHash());
-
-//            }
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally {
+            System.out.println("----------------------End of ServiceStationValidation/validateBlock---------------------------------");
         }
         return false;
-
-
     }
 
 }
